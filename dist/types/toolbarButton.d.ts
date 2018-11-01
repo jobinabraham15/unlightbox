9f74972dca6d922e9476e40d55206f20e618a10f
jobinabraham15 <jobin.abraham@aasaanjobs.com>
2018-11-01 12:17:22 +0530

    build added

--- dist/types/toolbarButton.d.ts
+++ dist/types/toolbarButton.d.ts
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { IToolbarButtonProps } from "./toolbarButton.interface";
+import "./index.css";
 export default class ToolbarButton extends React.Component<IToolbarButtonProps> {
     render(): JSX.Element;
 }
